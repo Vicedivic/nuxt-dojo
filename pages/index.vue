16124cd7b3ecceead5c54561183a629a96d8dcda
Victor Fink Danø <vic@revealit.dk>
2023-09-18 00:31:53 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Home</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis quasi tempore? Deserunt hic natus
+    quas rem! Dolorum, natus, quaerat.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam perspiciatis quasi tempore? Deserunt hic natus
+    quas rem! Dolorum, natus, quaerat.</p>
+</template>
+
+<style scoped>
+h1 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
